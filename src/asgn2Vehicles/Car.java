fb132632e2ee7b662ede1af596d0ece88c660bb7
Ishaan <ishaan.singh@connect.qut.edu.au>
2014-05-26 02:31:15 +1000

    Vehicle, Car, MotorCycle class w/o Exceptions

--- src/asgn2Vehicles/Car.java
+++ src/asgn2Vehicles/Car.java
@@ -13,40 +13,63 @@ package asgn2Vehicles;
 import asgn2Exceptions.VehicleException;
 
 /**
- * The Car class is a specialisation of the Vehicle class to cater for production cars
- * This version of the class does not cater for model types, but records whether or not the 
- * vehicle can use a small parking space. 
+ * The Car class is a specialisation of the Vehicle class to cater for
+ * production cars This version of the class does not cater for model types, but
+ * records whether or not the vehicle can use a small parking space.
  * 
  * @author hogan
- *
+ * 
  */
 public class Car extends Vehicle {
-	
-	private boolean small; 
+
+	private boolean small;
 
 	/**
-	 * The Car Constructor - small set at creation, not mutable. 
-	 * @param vehID - identification number or plate of the vehicle
-	 * @param arrivalTime - time (minutes) at which the vehicle arrives and is 
-	 *        either queued or given entry to the carpark 
-	 * @param small - indicator whether car is regarded as small or not
-	 * @throws VehicleException if arrivalTime is <= 0  
+	 * The Car Constructor - small set at creation, not mutable.
+	 * 
+	 * @param vehID
+	 *            - identification number or plate of the vehicle
+	 * @param arrivalTime
+	 *            - time (minutes) at which the vehicle arrives and is either
+	 *            queued or given entry to the carpark
+	 * @param small
+	 *            - indicator whether car is regarded as small or not
+	 * @throws VehicleException
+	 *             if arrivalTime is <= 0
 	 */
-	public Car(String vehID, int arrivalTime, boolean small) throws VehicleException {
+	public Car(String vehID, int arrivalTime, boolean small)
+			throws VehicleException {
+		super(vehID, arrivalTime);
+		this.small = small;
 	}
 
 	/**
-	 * Boolean status indicating whether car is small enough for small 
-	 * car parking spaces  
+	 * Boolean status indicating whether car is small enough for small car
+	 * parking spaces
+	 * 
 	 * @return true if small parking space, false otherwise
 	 */
 	public boolean isSmall() {
+		return this.small;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see asgn2Vehicles.Vehicle#toString()
 	 */
 	@Override
 	public String toString() {
+		String str = "";
+		str += super.toString();
+		str += "\nCar ";
+		if (this.isSmall()) {
+			str += "can use small car ";
+		} else {
+			str += "cannot use small ";
+		}
+		str += "parking space";
+		
+		return str;
 	}
 }
--- src/asgn2Vehicles/Vehicle.java
+++ src/asgn2Vehicles/Vehicle.java
@@ -13,161 +13,256 @@ package asgn2Vehicles;
 import asgn2Exceptions.VehicleException;
 import asgn2Simulators.Constants;
 
-
-
 /**
- * Vehicle is an abstract class specifying the basic state of a vehicle and the methods used to 
- * set and access that state. A vehicle is created upon arrival, at which point it must either 
- * enter the car park to take a vacant space or become part of the queue. If the queue is full, then 
- * the vehicle must leave and never enters the car park. The vehicle cannot be both parked and queued 
- * at once and both the constructor and the parking and queuing state transition methods must 
- * respect this constraint. 
+ * Vehicle is an abstract class specifying the basic state of a vehicle and the
+ * methods used to set and access that state. A vehicle is created upon arrival,
+ * at which point it must either enter the car park to take a vacant space or
+ * become part of the queue. If the queue is full, then the vehicle must leave
+ * and never enters the car park. The vehicle cannot be both parked and queued
+ * at once and both the constructor and the parking and queuing state transition
+ * methods must respect this constraint.
  * 
- * Vehicles are created in a neutral state. If the vehicle is unable to park or queue, then no changes 
- * are needed if the vehicle leaves the carpark immediately.
- * Vehicles that remain and can't park enter a queued state via {@link #enterQueuedState() enterQueuedState} 
- * and leave the queued state via {@link #exitQueuedState(int) exitQueuedState}. 
- * Note that an exception is thrown if an attempt is made to join a queue when the vehicle is already 
- * in the queued state, or to leave a queue when it is not. 
+ * Vehicles are created in a neutral state. If the vehicle is unable to park or
+ * queue, then no changes are needed if the vehicle leaves the carpark
+ * immediately. Vehicles that remain and can't park enter a queued state via
+ * {@link #enterQueuedState() enterQueuedState} and leave the queued state via
+ * {@link #exitQueuedState(int) exitQueuedState}. Note that an exception is
+ * thrown if an attempt is made to join a queue when the vehicle is already in
+ * the queued state, or to leave a queue when it is not.
  * 
- * Vehicles are parked using the {@link #enterParkedState(int, int) enterParkedState} method and depart using 
- * {@link #exitParkedState(int) exitParkedState}
+ * Vehicles are parked using the {@link #enterParkedState(int, int)
+ * enterParkedState} method and depart using {@link #exitParkedState(int)
+ * exitParkedState}
  * 
- * Note again that exceptions are thrown if the state is inappropriate: vehicles cannot be parked or exit 
- * the car park from a queued state. 
+ * Note again that exceptions are thrown if the state is inappropriate: vehicles
+ * cannot be parked or exit the car park from a queued state.
  * 
- * The method javadoc below indicates the constraints on the time and other parameters. Other time parameters may 
- * vary from simulation to simulation and so are not constrained here.  
+ * The method javadoc below indicates the constraints on the time and other
+ * parameters. Other time parameters may vary from simulation to simulation and
+ * so are not constrained here.
  * 
  * @author hogan
- *
+ * 
  */
 public abstract class Vehicle {
-	
+
+	private String vehID;
+	private int arrivalTime, departureTime, parkingTime;
+	private boolean isParked, isQueued, isSatisfied, wasParked, wasQueued;
+
 	/**
-	 * Vehicle Constructor 
-	 * @param vehID String identification number or plate of the vehicle
-	 * @param arrivalTime int time (minutes) at which the vehicle arrives and is 
-	 *        either queued, given entry to the car park or forced to leave
-	 * @throws VehicleException if arrivalTime is <= 0 
+	 * Vehicle Constructor
+	 * 
+	 * @param vehID
+	 *            String identification number or plate of the vehicle
+	 * @param arrivalTime
+	 *            int time (minutes) at which the vehicle arrives and is either
+	 *            queued, given entry to the car park or forced to leave
+	 * @throws VehicleException
+	 *             if arrivalTime is <= 0
 	 */
-	public Vehicle(String vehID,int arrivalTime) throws VehicleException  {
+	public Vehicle(String vehID, int arrivalTime) throws VehicleException {
+		this.vehID = vehID;
+		this.arrivalTime = arrivalTime;
 	}
 
 	/**
-	 * Transition vehicle to parked state (mutator)
-	 * Parking starts on arrival or on exit from the queue, but time is set here
-	 * @param parkingTime int time (minutes) at which the vehicle was able to park
-	 * @param intendedDuration int time (minutes) for which the vehicle is intended to remain in the car park.
-	 *  	  Note that the parkingTime + intendedDuration yields the departureTime
-	 * @throws VehicleException if the vehicle is already in a parked or queued state, if parkingTime <= 0, 
-	 *         or if intendedDuration is less than the minimum prescribed in asgnSimulators.Constants
+	 * Transition vehicle to parked state (mutator) Parking starts on arrival or
+	 * on exit from the queue, but time is set here
+	 * 
+	 * @param parkingTime
+	 *            int time (minutes) at which the vehicle was able to park
+	 * @param intendedDuration
+	 *            int time (minutes) for which the vehicle is intended to remain
+	 *            in the car park. Note that the parkingTime + intendedDuration
+	 *            yields the departureTime
+	 * @throws VehicleException
+	 *             if the vehicle is already in a parked or queued state, if
+	 *             parkingTime <= 0, or if intendedDuration is less than the
+	 *             minimum prescribed in asgnSimulators.Constants
 	 */
-	public void enterParkedState(int parkingTime, int intendedDuration) throws VehicleException {
+	public void enterParkedState(int parkingTime, int intendedDuration)
+			throws VehicleException {
+		this.parkingTime = parkingTime;
+		this.departureTime = this.parkingTime + intendedDuration;
+		this.isParked = true;
 	}
-	
+
 	/**
-	 * Transition vehicle to queued state (mutator) 
-	 * Queuing formally starts on arrival and ceases with a call to {@link #exitQueuedState(int) exitQueuedState}
-	 * @throws VehicleException if the vehicle is already in a queued or parked state
+	 * Transition vehicle to queued state (mutator) Queuing formally starts on
+	 * arrival and ceases with a call to {@link #exitQueuedState(int)
+	 * exitQueuedState}
+	 * 
+	 * @throws VehicleException
+	 *             if the vehicle is already in a queued or parked state
 	 */
 	public void enterQueuedState() throws VehicleException {
-	}
-	
-	/**
-	 * Transition vehicle from parked state (mutator) 
-	 * @param departureTime int holding the actual departure time 
-	 * @throws VehicleException if the vehicle is not in a parked state, is in a queued 
-	 * 		  state or if the revised departureTime < parkingTime
-	 */
-	public void exitParkedState(int departureTime) throws VehicleException {
+		this.isQueued = true;
 	}
 
 	/**
-	 * Transition vehicle from queued state (mutator) 
-	 * Queuing formally starts on arrival with a call to {@link #enterQueuedState() enterQueuedState}
-	 * Here we exit and set the time at which the vehicle left the queue
-	 * @param exitTime int holding the time at which the vehicle left the queue 
-	 * @throws VehicleException if the vehicle is in a parked state or not in a queued state, or if 
-	 *  exitTime is not later than arrivalTime for this vehicle
+	 * Transition vehicle from parked state (mutator)
+	 * 
+	 * @param departureTime
+	 *            int holding the actual departure time
+	 * @throws VehicleException
+	 *             if the vehicle is not in a parked state, is in a queued state
+	 *             or if the revised departureTime < parkingTime
+	 */
+	public void exitParkedState(int departureTime) throws VehicleException {
+		this.departureTime = departureTime;
+		this.isParked = false;
+		this.wasParked = true;
+	}
+
+	/**
+	 * Transition vehicle from queued state (mutator) Queuing formally starts on
+	 * arrival with a call to {@link #enterQueuedState() enterQueuedState} Here
+	 * we exit and set the time at which the vehicle left the queue
+	 * 
+	 * @param exitTime
+	 *            int holding the time at which the vehicle left the queue
+	 * @throws VehicleException
+	 *             if the vehicle is in a parked state or not in a queued state,
+	 *             or if exitTime is not later than arrivalTime for this vehicle
 	 */
 	public void exitQueuedState(int exitTime) throws VehicleException {
+		this.isQueued = false;
+		this.wasQueued = true;
+		this.parkingTime = exitTime;
 	}
-	
+
 	/**
-	 * Simple getter for the arrival time 
+	 * Simple getter for the arrival time
+	 * 
 	 * @return the arrivalTime
 	 */
 	public int getArrivalTime() {
+		return this.arrivalTime;
 	}
-	
+
 	/**
-	 * Simple getter for the departure time from the car park
-	 * Note: result may be 0 before parking, show intended departure 
-	 * time while parked; and actual when archived
+	 * Simple getter for the departure time from the car park Note: result may
+	 * be 0 before parking, show intended departure time while parked; and
+	 * actual when archived
+	 * 
 	 * @return the departureTime
 	 */
 	public int getDepartureTime() {
+		return this.departureTime;
 	}
-	
+
 	/**
-	 * Simple getter for the parking time
-	 * Note: result may be 0 before parking
+	 * Simple getter for the parking time Note: result may be 0 before parking
+	 * 
 	 * @return the parkingTime
 	 */
 	public int getParkingTime() {
+		return this.parkingTime;
 	}
 
 	/**
 	 * Simple getter for the vehicle ID
+	 * 
 	 * @return the vehID
 	 */
 	public String getVehID() {
+		return this.vehID;
 	}
 
 	/**
-	 * Boolean status indicating whether vehicle is currently parked 
+	 * Boolean status indicating whether vehicle is currently parked
+	 * 
 	 * @return true if the vehicle is in a parked state; false otherwise
 	 */
 	public boolean isParked() {
+		return this.isParked;
 	}
 
 	/**
 	 * Boolean status indicating whether vehicle is currently queued
-	 * @return true if vehicle is in a queued state, false otherwise 
+	 * 
+	 * @return true if vehicle is in a queued state, false otherwise
 	 */
 	public boolean isQueued() {
+		return this.isQueued;
 	}
-	
+
 	/**
-	 * Boolean status indicating whether customer is satisfied or not
-	 * Satisfied if they park; dissatisfied if turned away, or queuing for too long 
-     * Vehicles begin in a satisfied state, but this may change over time
-	 * Note that calls to this method may not reflect final status 
-	 * @return true if satisfied, false if never in parked state or if queuing time exceeds max allowable 
+	 * Boolean status indicating whether customer is satisfied or not Satisfied
+	 * if they park; dissatisfied if turned away, or queuing for too long
+	 * Vehicles begin in a satisfied state, but this may change over time Note
+	 * that calls to this method may not reflect final status
+	 * 
+	 * @return true if satisfied, false if never in parked state or if queuing
+	 *         time exceeds max allowable
 	 */
 	public boolean isSatisfied() {
+		if (this.wasQueued && !this.wasParked) {
+			this.isSatisfied = false;
+		} else if (!this.wasQueued && !this.wasParked) {
+			this.isSatisfied = false;
+		} else {
+			this.isSatisfied = true;
+		}
+
+		return this.isSatisfied;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
+		int queueTime = this.getParkingTime() - this.getArrivalTime();
+
+		String str = "";
+		str += "Vehicle vehID: " + this.getVehID();
+		str += "\nArrival Time: " + this.getArrivalTime();
+		if (this.wasQueued()) {
+			str += "\nExit from Queue: " + this.getParkingTime();
+			str += "\nQueuing Time: " + (queueTime);
+			if (queueTime > Constants.MAXIMUM_QUEUE_TIME) {
+				str += "\nExceeded maximum acceptable queuing time by: "
+						+ (queueTime - Constants.MAXIMUM_QUEUE_TIME);
+			}
+		} else {
+			str += "\nVehicle was not queued";
+		}
+		if (this.wasParked()) {
+			str += "\nEntry to Car Park: " + this.getParkingTime();
+			str += "\nExit from Car Park: " + this.getDepartureTime();
+			str += "\nParking Time: "
+					+ (this.getDepartureTime() - this.getParkingTime());
+		} else {
+			str += "\nVehicle was not parked";
+		}
+		str += "\nCustomer was ";
+		if (!this.isSatisfied()) {
+			str += "not ";
+		}
+		str += "satisfied";
+		return str;
 	}
 
 	/**
-	 * Boolean status indicating whether vehicle was ever parked
-	 * Will return false for vehicles in queue or turned away 
-	 * @return true if vehicle was or is in a parked state, false otherwise 
+	 * Boolean status indicating whether vehicle was ever parked Will return
+	 * false for vehicles in queue or turned away
+	 * 
+	 * @return true if vehicle was or is in a parked state, false otherwise
 	 */
 	public boolean wasParked() {
+		return this.wasParked;
 	}
 
 	/**
 	 * Boolean status indicating whether vehicle was ever queued
-	 * @return true if vehicle was or is in a queued state, false otherwise 
+	 * 
+	 * @return true if vehicle was or is in a queued state, false otherwise
 	 */
 	public boolean wasQueued() {
+		return this.wasQueued;
 	}
 }
